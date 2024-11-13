3208fee50351ac76b43489aab0e5de8f2ce55e6a
duncan <dwerner@riskamp.com>
2024-11-13 11:28:58 -0800

    remove files

--- node_modules/astro/dist/core/middleware/noop-middleware.js
+++
@@ -1,8 +0,0 @@
-import { NOOP_MIDDLEWARE_HEADER } from "../constants.js";
-const NOOP_MIDDLEWARE_FN = (ctx, next) => {
-  ctx.request.headers.set(NOOP_MIDDLEWARE_HEADER, "true");
-  return next();
-};
-export {
-  NOOP_MIDDLEWARE_FN
-};
